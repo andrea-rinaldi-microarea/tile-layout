0d7e19390a7dfd29cb10f9d2074ee6f85fce996c
Daaren Urthling <daaren.urthling@hotmail.com>
2020-05-01 19:28:45 +0200

    posizionamento campi

--- fee-templates/fee-templates.js
+++ fee-templates/fee-templates.js
@@ -3,29 +3,27 @@ var feeTemplates = {
 
 ( feeTemplates => {
     
-    $("#lc1").append(utils.render(tileTemplate, {
-        "title": "Main Data"
-    }));
-    $("#lc1").append(utils.render(tileTemplate, {
-        "title": "Percentages"
-    }));
+    utils.addTile($("#lc1"), "IDD_TD_FEETEMPLATES_MAIN_DATA", "Main Data");
+    utils.addFields($("#IDD_TD_FEETEMPLATES_MAIN_DATA"), [
+        utils.createField("Template", 3, 3),
+        utils.createField("Description", 3, 7)
+    ]);
+        
+    utils.addTile($("#lc1"), "IDD_TD_FEETEMPLATES_PERCENTAGES", "Percentages");
+    utils.addFields($("#IDD_TD_FEETEMPLATES_PERCENTAGES"), [
+        utils.createField("Tax %", 3, 2),
+        utils.createRow([
+            utils.createField("WT %", 3, 2),
+            utils.createField("", 0, 3, "enum")    
+        ]),
+        utils.createField("WT Base", 3, 2)
+    ]);
 
+    utils.addTile($("#lc2"), "IDD_TD_FEETEMPLATES_DECLA_DATA", "Declaration data");
+    utils.addTile($("#lc2"), "IDD_TD_FEETEMPLATES_INPS", "INPS");
 
-    $("#lc2").append(utils.render(tileTemplate, {
-        "title": "Declaration data"
-    }));
-    $("#lc2").append(utils.render(tileTemplate, {
-        "title": "INPS"
-    }));
+    utils.addTile($("#lc3"), "IDD_TD_FEETEMPLATES_ENASARCO", "ENASARCO");
 
-
-    $("#lc3").append(utils.render(tileTemplate, {
-        "title": "ENASARCO"
-    }));
-
-
-    $("#lc4").append(utils.render(tileTemplate, {
-        "title": "details"
-    }));
+    utils.addTile($("#lc4"), "IDD_TD_FEETEMPLATES_DETAIL", "details");
 
 }) (feeTemplates);
--- utils.js
+++ utils.js
@@ -1,5 +1,9 @@
 var utils = {
-    render(template, props) {}
+    render(template, props) {},
+    addTile(elem, id, title) {},
+    addFields(tile, fields) {},
+    createField(label, captionSize, controlSize) {},
+    createRow(fields) {}
 };
 ( utils => {
 
@@ -10,5 +14,49 @@ var utils = {
       
         return template;
     }
+
+    utils.addTile = function(elem, id, title) {
+        var tile = $(utils.render(tileTemplate, {
+            "title": title
+        }));
+        tile.attr("id", id);
+        elem.append(tile);
+    }
+
+    utils.createField = function(caption, captionSize, controlSize, controlType) {
+        var field = $(utils.render(tileControlTemplate, {
+            "caption": caption
+        }));
+        field.children(".tile-control").addClass("size-" + controlSize);
+        field.find(".tile-control :input").hide();
+        if (controlType == "enum") {
+            field.find(".tile-control .enum").show();
+        } else if (controlType == "combo") {
+            field.find(".tile-control .combo").show();
+        } else {
+            field.find(".tile-control input").show();
+        }
+        if (captionSize > 0) {
+            field.children(".tile-control-caption").addClass("size-" + captionSize);
+        } else {
+            field.children(".tile-control-caption").hide();
+        }
+        return field;
+    }
+
+    utils.addFields = function(tile, fields) {
+        fields.forEach(field => {
+            tile.append(field);
+        });
+    }
+
+    utils.createRow = function(fields) {
+        var row = $(tileRowTemplate);
+        fields.forEach(field => {
+            row.append(field);
+        });
+        return row;
+    }
+
 
 })(utils);
