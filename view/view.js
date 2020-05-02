b047e5ee869ad8dcb05b8e6739b2d4e266ce1b28
Daaren Urthling <daaren.urthling@hotmail.com>
2020-05-02 12:08:36 +0200

    display seconda colonna wide

--- view/view.js
+++ view/view.js
@@ -37,21 +37,42 @@ var view = {
         return field;
     }
 
+    function createColumn(tag, jsonTile) {
+        if (!jsonTile.items) return;
+        var col = $(tileColumnTemplate);
+        jsonTile.items.filter(i => i.anchor == tag).forEach(item => {
+            var fields = [ createField(item) ];
+            jsonTile.items.filter(i => i.anchor == item.id).forEach(anchored => {
+                fields.push(createField(anchored));
+            });
+            col.append(createRow(fields));
+        });
+        if (col.children().length > 0)
+            return col;
+    }
+
     view.addTile = function(elem, jsonTile) {
         var tile = $(utils.render(tileTemplate, {
             "title": jsonTile.text
         }));
         tile.attr("id", jsonTile.id);
-        if (jsonTile.items) {
-            jsonTile.items.forEach(item => {
-                if (item.anchor != "COL1" && item.anchor != "COL2" && item.anchor != "")
-                    return;
-                var fields = [ createField(item) ];
-                jsonTile.items.filter(i => i.anchor == item.id).forEach(anchored => {
-                    fields.push(createField(anchored));
-                });
-                tile.append(createRow(fields));
-            });
+        // if (jsonTile.items) {
+        //     var col1 = $(tileColumnTemplate);
+        //     jsonTile.items.filter(i => i.anchor == "COL1").forEach(item => {
+        //         if (item.anchor != "COL1" && item.anchor != "COL2" && item.anchor != "")
+        //             return;
+        //         var fields = [ createField(item) ];
+        //         jsonTile.items.filter(i => i.anchor == item.id).forEach(anchored => {
+        //             fields.push(createField(anchored));
+        //         });
+        //         col1.append(createRow(fields));
+        //     });
+        // }
+        for (c = 1; c <= 4; c++) {
+            var col = createColumn("COL" + c, jsonTile);
+            if (!col)
+                break;
+            tile.children(".tile-content").append(col);
         }
         elem.append(tile);
     }
