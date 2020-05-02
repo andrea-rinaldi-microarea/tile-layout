88ec5f21fb56a8bf3321f912284dc5b57731787f
Daaren Urthling <daaren.urthling@hotmail.com>
2020-05-02 21:25:01 +0200

    switch country

--- carriers/carriers.js
+++ carriers/carriers.js
@@ -3,16 +3,46 @@ var carriers = {
 
 ( carriers => {
     
-    view.addView(
+    var tiles =  [
+        IDD_TD_CARRIERS_MAIN,
+        IDD_TD_CARRIERS_CONTACTS,
+        //IDD_TD_CARRIERS_ADDRESS_BR,
+        IDD_TD_CARRIERS_ADDRESS,
+        IDD_TD_CARRIERS_CHARGES_CONDITION,
+        IDD_TD_CARRIERS_REGISTERS,
+        IDD_TD_CARRIERS_TRANSPORTATION_FORM
+    ];
+    var countryBR = false;
+
+    function loadView() {
+        view.addView(
             $("#mainView"), 
             IDD_CARRIERS_VIEW,
-            [
-                IDD_TD_CARRIERS_MAIN,
-                IDD_TD_CARRIERS_CONTACTS,
-                IDD_TD_CARRIERS_ADDRESS,
-                IDD_TD_CARRIERS_CHARGES_CONDITION,
-                IDD_TD_CARRIERS_REGISTERS,
-                IDD_TD_CARRIERS_TRANSPORTATION_FORM
-            ]);
+            tiles
+        );
+    }
+    loadView();
+
+    function removeTile(tile) {
+        var idx = tiles.findIndex( t => t.id == tile.id);
+        if (idx != -1) {
+            tiles.splice(idx,1);
+        }
+    }
+
+    $("#switch-panel-country").click(function() {
+        if (!countryBR) {
+            removeTile(IDD_TD_CARRIERS_ADDRESS);
+            removeTile(IDD_TD_CARRIERS_REGISTERS);
+            tiles.push(IDD_TD_CARRIERS_ADDRESS_BR);
+        } else {
+            removeTile(IDD_TD_CARRIERS_ADDRESS_BR);
+            tiles.push(IDD_TD_CARRIERS_ADDRESS);
+            tiles.push(IDD_TD_CARRIERS_REGISTERS);
+        }
+        countryBR = !countryBR;
+        $("#mainView")[0].innerHTML = "";
+        loadView();
+});
 
 }) (carriers);
