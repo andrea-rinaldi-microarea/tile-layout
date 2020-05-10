6a25f79251ab509c042a731a8f098381a2982fe1
Daaren Urthling <daaren.urthling@hotmail.com>
2020-05-10 08:34:38 +0200

    migliorato switch panel

--- switch-panel/switch-panel.js
+++ switch-panel/switch-panel.js
@@ -1,17 +1,34 @@
 var switchPanel = {
-
+    up: function() {},
+    down: function() {},
+    auto: function() {}
 };
 
 (switchPanel => {
     $("body").prepend($(switchPanelForm));
-    var radio = $('#switch-panel-screen-size input:radio');
-    radio.click(function() {
-        if (this.value == "") {
-            $("#mainView")[0].style.maxWidth= "";
-        } else {
-            $("#mainView").css({
-                "maxWidth": this.value
-              });            
-        }
+
+    var currSize = -1;
+    var screenSizes = [ '400px', '540px', '720px', '960px', '1140px' ];
+    switchPanel.up = function() {
+        if (currSize == screenSizes.length - 1) return;
+        if (currSize == -1) currSize = screenSizes.length - 2;
+        document.documentElement.style.setProperty('--max-width', screenSizes[++currSize]);
+        $("#switch-panel-current-size")[0].innerHTML = screenSizes[currSize];
+    }
+    switchPanel.down = function() {
+        if (currSize == 0) return;
+        if (currSize == -1) currSize = screenSizes.length - 1;
+        document.documentElement.style.setProperty('--max-width', screenSizes[--currSize]);
+        $("#switch-panel-current-size")[0].innerHTML = screenSizes[currSize];
+    }
+    switchPanel.auto = function() {
+        currSize = -1;
+        document.documentElement.style.setProperty('--max-width', 'inherit');
+        $("#switch-panel-current-size")[0].innerHTML = "auto";
+    }
+    $("#switch-panel-browser-size")[0].innerHTML = `(browser: ${$(window).width()})`;
+    $(window).resize(function() {
+        $("#switch-panel-browser-size")[0].innerHTML = `(browser: ${$(window).width()})`;
     });
+
 })(switchPanel);
