03d43be3b41e5c687e626f04ffdec13d2e1acff5
Daaren Urthling <daaren.urthling@hotmail.com>
2020-05-10 08:37:10 +0200

    Nuovi test su view

---
+++ test/test.js
@@ -0,0 +1,16 @@
+var test = {
+};
+
+( test => {
+
+    var tiles =  [
+        IDD_TD_TEST_MISC
+    ];
+
+    view.addView(
+        $("#mainView"), 
+        IDD_TEST_VIEW,
+        tiles
+    );
+
+})(test);
